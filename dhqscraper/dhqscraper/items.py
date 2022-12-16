2a052be3bee4e0e7f8ede7843604cb901e14990a
David Beales <rdbeales@gmail.com>
2022-12-16 10:40:25 -0500

    Update items.py

--- dhqscraper/dhqscraper/items.py
+++ dhqscraper/dhqscraper/items.py
@@ -4,9 +4,20 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader import ItemLoader
+from itemloaders.processors import TakeFirst, MapCompose
+from w3lib.html import remove_tags
+import re
+
+def remove_multispaces(value):
+    return re.sub.value.strip()    
+#.replace('\n', ' ').replace('\t','')
 
 
 class DhqscraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    title = scrapy.Field()
+    author = scrapy.Field()
+    pubinfo = scrapy.Field()
+    xmllink = scrapy.Field()
     pass
